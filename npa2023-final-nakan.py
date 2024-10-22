61b5c020630e796118b9f5e96749d8b88abefec6
nakan ponboon <65070058@kmitl.ac.th>
2024-10-22 19:13:34 +0700

    no.7 lat lon

--- npa2023-final-nakan.py
+++ npa2023-final-nakan.py
@@ -99,8 +99,9 @@ while True:
 #######################################################################################
 # 7. Provide the OpenWeather Geocoding key values for latitude and longitude.
         # Set the lat and lng key as retuned by the OpenWeather Geocoding API in variables
-        locationLat = json_data["<!!!REPLACEME!!!> with path to latitude key!!!>"]
-        locationLng = json_data["<!!!REPLACEME!!!> with path to longitude key!!!>"]
+        locationLat = json_data[0]["lat"]
+        locationLng = json_data[0]["lon"]
+        print(locationLat, locationLng)
 
 # #######################################################################################
 # # 8. Prepare openweatherAPIGetParameters for OpenWeather API, https://openweathermap.org/api; current weather data for one location by geographic coordinates.
